f6c158193e8e55ac5996ff2479e2870cbeefbb46
Domenic Franjic <105493643+SiSanchopancho@users.noreply.github.com>
2023-12-20 17:33:28 +0100

    A quarter past half

---
+++ R/usethis.R
@@ -0,0 +1,32 @@
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+#' @useDynLib KFSMLE
+## usethis namespace: end
+NULL
+
+#' Simulated data set for functionality tests testing
+#'
+#' Some simulated data drawn from a diffuse state-space model
+#'
+#' @format A data frame with 1000 rows and 4 columns
+#' @source Simulated
+#' @name data
+NULL
+
+#' Starting vector
+#'
+#' A starting vector for the MLE algorithm
+#'
+#' @format A data frame with 15 rows and 1 column
+#' @source Simulated
+#' @name start
+NULL
+
+#' Parameter vector
+#'
+#' A vector of additional parameters
+#'
+#' @format A data frame with 1000 rows and 1 column
+#' @source Simulated
+#' @name tau
+NULL
